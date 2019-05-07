5cb15b92d931e5371a8128b3bd82a0c8465407d0
Jackie Chui <jackie2009hk@gmail.com>
2019-05-07 02:27:30 -0700

    Removed keyboard shortcut

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const pasteToAll = (pasteOverSelection, event) => {
-	const selectedFrames = pasteOverSelection
-		? figmaPlus.currentPage.selection
-		: figmaPlus.currentPage.selection.filter(selection => selection.type === 'FRAME');
-	if (pasteOverSelection && event.type === 'keydown') App.triggerAction('delete-selection');
+const paste = () => {
+	const selectedFrames = figmaPlus.currentPage.selection.filter(selection => selection.type === 'FRAME');
+	if (selectedFrames.length === 0) {
+		figmaPlus.showToast({ message: 'Please select one or more frames to paste into' });
+	}
 	for (let selectedFrame of selectedFrames) {
 		figmaPlus.currentPage.selection = [selectedFrame];
-		pasteOverSelection ? App.triggerAction('paste-over-selection') : App.triggerAction('paste');
+		App.triggerAction('paste');
+	}
+};
+
+const pasteOverSelection = () => {
+	const selectedFrames = figmaPlus.currentPage.selection;
+	for (let selectedFrame of selectedFrames) {
+		figmaPlus.currentPage.selection = [selectedFrame];
+		App.triggerAction('paste-over-selection');
 	}
 };
 
@@ -15,39 +23,13 @@ figmaPlus.addCommand({
 	submenu: [
 		{
 			label: 'Paste in Place',
-			action: e => pasteToAll(false, e),
-			condition: () =>
-				figmaPlus.currentPage.selection.length > 0 &&
-				figmaPlus.currentPage.selection.every(node => node.type === 'FRAME'),
-			shortcut: {
-				mac: {
-					command: true,
-					key: 'V'
-				},
-				windows: {
-					control: true,
-					key: 'V'
-				}
-			}
+			condition: () => figmaPlus.currentPage.selection.some(node => node.type === 'FRAME'),
+			action: e => paste()
 		},
 		{
 			label: 'Paste Over Selection',
-			action: e => pasteToAll(true, e),
-			condition: () =>
-				figmaPlus.currentPage.selection.length > 0 &&
-				figmaPlus.currentPage.selection.every(node => node.type === 'FRAME'),
-			shortcut: {
-				mac: {
-					command: true,
-					shift: true,
-					key: 'V'
-				},
-				windows: {
-					control: true,
-					shift: true,
-					key: 'V'
-				}
-			}
+			action: e => pasteOverSelection()
 		}
-	]
+	],
+	showInSelectionMenu: true
 });
